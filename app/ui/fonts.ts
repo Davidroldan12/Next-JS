cb0c91a51ce95734d569b5c25759ab51430adc53
Davidroldan12 <168463190+Davidroldan12@users.noreply.github.com>
2024-11-14 19:21:00 -0500

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// En etse archivo se manejaran todas las fuentes de texto
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
